2b783fdedccf4edfe30f9b4255ff87940a5ac7e2
NIKHARshop <131625048+NIKHARshop@users.noreply.github.com>
2024-09-07 19:39:08 -0500

    1

---
+++ scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $('.carousel').carousel({
+        interval: 3000
+    });
+});
+
+$(document).ready(function(){
+    $('.carousel').carousel({
+        interval: 5000
+    });
+});
+$(document).ready(function(){
+    $('#cardSlider').carousel({
+        interval: 3000 // Cambia la imagen cada 3 segundos
+    });
+});
